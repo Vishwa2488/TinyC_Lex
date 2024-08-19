19e5ed6972f27a5668599cbac3debb611e70bbf0
Vishwa <117110630+Vishwa2488@users.noreply.github.com>
2024-08-19 16:40:46 +0530

    Punctuations, identifiers, string literal, constants

---
+++ test.c
@@ -0,0 +1,54 @@
+0
+
+
+15315.23142353e+54
+
+15315.23142353E-54
+
+15315.23142353e54
+' '
+'4'
+'q' 'w' 'e' 'r' 'r' '2' 'f' ' ''e'
+15315.23142353E54
+
+0000001.000000000E54
+0000001.000000000e54
+
+15315.23142353e+54
+
+
+124234
+for int long short 
+auto enum restrict unsigned
+break extern return void
+case float short volatile
+char for signed while
+const goto sizeof Bool
+continue if static Complex
+default inline struct Imaginary
+do int switch
+double long typedef
+else register union
+
+/*abclghasrg9 w84ht iugn w t94q3h tvohb
+efncw389th4 9b49 58h498h9 */
+
+// hello123
+
+"hello world"
+
+"hello ithgq549v834htvmc09uv5c98q2t9y38q t89 y9835v98m35y 8953qy u89vv hywh 54o9h8w5yo8 hy5y3q  h 35uy 983w y98432q yh8935qy h9835 yh98 35hworld"
+
+___identifer583846534thfesiugvehwrt875gqev87gq4b87rvfq8________________fbc87q34r38745
+
+{
+
+}
+
+[
+
+]
+
+(
+    
+)
---
+++ tester.c
@@ -0,0 +1,24 @@
+
+
+int main(int argc, char const *argv[])
+{
+    int x = 00001.23524034235023052305000250435e11342;
+    int w = 135833;
+    float y = 543;
+    double z = 00001.235240342350230523050435e11342;
+    int a = x + y ;
+    int b = x & w ;
+    int c = 0    ;
+    c++  ;
+    c += a;
+
+    printf("%d %f %lf", x, y, z);
+
+    s_char ______s________ = "1423o85y3443twhgi3h 4tv893qyv69m2 3y5t9q3m ty93q58y9tq53y65y983q4oy89h4"; 
+    c_char __s13534theirghetq34t347thgeb7 = 'a', ______s48th3498vht34w98htv2 = '\n';
+    /* hfaertu9hq4398t hq39tg bwg 9mh983wgqG QRIGHWiehbksrn f
+    erugb wirgbq9th43q8 t93h5th58ghb4o 8h4wth b4tnh ieu4ghierun9wh rge\n\n
+    wjgbei eriguhi gu***********/ /**** 
+    */
+    return 0;
+}
